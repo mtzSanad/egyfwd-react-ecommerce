7cdff56666bca0789bed8d75ff8a7bb7b083db35
mtzSanad <mtz.sanad@gmail.com>
2022-12-02 21:41:26 +0200

    Adding items from list using map

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const Menu = () => {
+  const menuItems = ['Home', 'Add Item', 'Search', 'About'];
   return (
     <nav>
       <ul className="text-stone-100 text-lg my-2">
-        <li>Home</li>
-        <li>Add Item</li>
-        <li>Search</li>
-        <li>Abuot</li>
+        {menuItems.map((e) => (
+          <li className="my-2 font-bold" key={e}>
+            {e}
+          </li>
+        ))}
       </ul>
     </nav>
   );
