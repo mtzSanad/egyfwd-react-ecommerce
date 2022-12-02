cd9a3fffafef7a1cdbe1ea442c34a39333eef631
mtzSanad <mtz.sanad@gmail.com>
2022-12-02 20:50:06 +0200

    adding App.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render('Hello', document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//! Testing render hello
+//root.render(<h1>Hello</h1>);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
