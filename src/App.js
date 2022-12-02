20e9db508ab31a0c2a34fea5a109c0386fabf984
mtzSanad <mtz.sanad@gmail.com>
2022-12-02 21:38:23 +0200

    adding menu static

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './component/Header';
+import Menu from './component/Menu';
 
 const App = () => {
   //Adding the basic component layout
@@ -7,7 +8,9 @@ const App = () => {
     <div className="bg-slate-800 h-screen">
       <Header title="e-Commerce" />
       <div className="container mx-auto grid grid-cols-12 gap-1">
-        <div className="col-span-2">Menu</div>
+        <div className="col-span-2">
+          <Menu />
+        </div>
         <div className="col-span-1">Main</div>
       </div>
     </div>
---
+++ src/component/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <nav>
+      <ul className="text-stone-100 text-lg my-2">
+        <li>Home</li>
+        <li>Add Item</li>
+        <li>Search</li>
+        <li>Abuot</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
